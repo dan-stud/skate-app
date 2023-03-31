cb5a0086678ba71ed6bde9c04ecb3cf660b745af
DanStuddersRS <daniel.studders@rs-components.com>
2023-03-31 16:37:07 +0100

    fix players not rotating

--- components/player/player.tsx
+++ components/player/player.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { PLAYER_TAG } from "../../pages/game";
 import Letter from "../letter/letter";
 import useAutosizeTextArea from "../../hooks/useAutosizeTextArea";
@@ -13,11 +13,18 @@ function PlayerSection(props: Props) {
   const [value, setValue] = useState("");
   const [active, setActive] = useState(false);
 
-  const playersName = props.name ? props.name : "Player Name";
-
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
 
   useAutosizeTextArea(textAreaRef.current, value);
+
+  useEffect(() => {
+    const textArea = document.getElementById(
+      "player-name-text" + props.id
+    ) as HTMLTextAreaElement;
+    if (textArea) {
+      textArea.textContent = props.name;
+    }
+  }, [props]);
 
   const handleChange = (evt: React.ChangeEvent<HTMLTextAreaElement>) => {
     const val = evt.target?.value;
@@ -26,26 +33,25 @@ function PlayerSection(props: Props) {
     setValue(val);
   };
 
-  const playerOut = (evt: React.ChangeEvent<HTMLButtonElement>) => {
-    if (evt.target?.value;
+  // const playerOut = (evt: React.ChangeEvent<HTMLButtonElement>) => {
+  //   if (evt.target?.value;
 
-    setActive(true);
-  };
+  //   setActive(true);
+  // };
 
   return (
     <div className="inline-grid grid-cols-6 align-middle px-2 py-3 mx-2 my-2 bg-white shadow-2xl rounded-md bg-opacity-95 border-2 border-gray-300">
-      <Letter disabled={!active} value="S" toggle={props.toggled}></Letter>
-      <Letter disabled={!active} value="K" toggle={props.toggled}></Letter>
-      <Letter disabled={!active} value="A" toggle={props.toggled}></Letter>
-      <Letter disabled={!active} value="T" toggle={props.toggled}></Letter>
-      <Letter out={!lost} disabled={!active} value="E" toggle={props.toggled}></Letter>
+      <Letter value="S" toggle={props.toggled}></Letter>
+      <Letter value="K" toggle={props.toggled}></Letter>
+      <Letter value="A" toggle={props.toggled}></Letter>
+      <Letter value="T" toggle={props.toggled}></Letter>
+      <Letter value="E" toggle={props.toggled}></Letter>
       <textarea
-        id="player-name-text"
+        id={"player-name-text" + props.id}
         onChange={handleChange}
         className="text-sm text-center pt-6 font-dafoe bg-transparent resize-none font-sedge"
         ref={textAreaRef}
         rows={1}
-        defaultValue={playersName}
       />
     </div>
   );
